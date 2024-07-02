3bb6b55061ef22e11922f20bc5784c1c4456f786
jpmono416 <jpmono416@gmail.com>
2024-07-02 19:50:44 +0100

    [Refactor]: Modified style on the DisasterPreviewCards

--- volunteer-coordination-fe-server/src/components/disasters/DisasterPreviewRow.jsx
+++ volunteer-coordination-fe-server/src/components/disasters/DisasterPreviewRow.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TrackedDisasterPreviewCard from "./TrackedDisasterPreviewCard";
 import { Col, Row } from "react-bootstrap";
 import CustomHeader from "../shared/CustomHeader";
 
 const DisasterPreviewRow = ({ disasters }) => {
+    const [maxHeight, setMaxHeight] = useState(0);
+
+    // Keep all cards the same height
+    const updateMaxHeight = (newHeight) => {
+        if (newHeight > maxHeight) {
+            setMaxHeight(newHeight);
+        }
+    };
+
     return (
         <>
             <CustomHeader>Tracked disasters:</CustomHeader>
             <Row>
                 {disasters.map((disaster) => (
-                    <Col key={disaster._id} sm={6} lg={3}>
-                        <TrackedDisasterPreviewCard disaster={disaster} />
+                    <Col key={disaster._id} sm={6} lg={3} style={{ minHeight: `${maxHeight}px`, display: 'flex', justifyContent: 'center' }}>
+                        <TrackedDisasterPreviewCard
+                            disaster={disaster}
+                            updateMaxHeight={updateMaxHeight}
+                        />
                     </Col>
                 ))}
             </Row>
--- volunteer-coordination-fe-server/src/components/disasters/TrackedDisasterPreviewCard.jsx
+++ volunteer-coordination-fe-server/src/components/disasters/TrackedDisasterPreviewCard.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import StatusBadge from "../shared/StatusBadge";
 
-const TrackedDisasterPreviewCard = ({ disaster }) => {
+const TrackedDisasterPreviewCard = ({ disaster, maxHeight, updateMaxHeight }) => {
+    const cardRef = useRef(null);
+
+    useEffect(() => {
+        if (cardRef.current) {
+            const currentHeight = cardRef.current.offsetHeight;
+            updateMaxHeight(currentHeight);
+        }
+    }, [updateMaxHeight]);
+
     return (
-        <Link to={`/disasters/${disaster._id}`} style={{ textDecoration: "none" }}>
+        <Link
+            to={`/disasters/${disaster._id}`}
+            style={{ textDecoration: "none", margin: "10px 0" }}
+        >
             <div
+                ref={cardRef}
                 style={{
-                    width: "20vw",
-                    height: "20vw",
-                    margin: "10px 0",
+                    //? This ensures responsiveness while having 150px as the max width
+                    //? it's a high vw value for it to always hit maxWidth on mobiles and not look than 150px
+                    width: "40vw",
+                    height: "100%",
                     maxWidth: "150px",
-                    maxHeight: "150px",
                     minWidth: "100px",
                     minHeight: "100px",
-                    borderRadius: "20px", // Adjusted for a card-like appearance
+                    borderRadius: "20px",
                     display: "flex",
                     flexDirection: "column",
                     justifyContent: "center",
@@ -22,13 +36,14 @@ const TrackedDisasterPreviewCard = ({ disaster }) => {
                     padding: "20px",
                     boxSizing: "border-box",
                     cursor: "pointer",
-                    backgroundColor: "#fff", // Optional: Added background color for contrast
+                    backgroundColor: "#fff",
                 }}
             >
                 <h4 style={{ margin: "0", fontSize: "1em", color: "black" }}>
                     {disaster.location}
                 </h4>
-                <h6 style={{ margin: "0", color: "grey" }}>{disaster.description}</h6>
+                <h6 style={{ margin: "10px 0", color: "grey" }}>{disaster.description}</h6>
+                <StatusBadge status={disaster.status} />
             </div>
         </Link>
     );
